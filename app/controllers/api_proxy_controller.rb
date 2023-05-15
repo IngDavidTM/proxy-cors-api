20fec72726a12f08db46aeaf5c7d4fbe53d0cd2d
IngDavidTM <david-1011@hotmail.com>
2023-05-15 17:01:37 -0500

    Change headers

--- app/controllers/api_proxy_controller.rb
+++ app/controllers/api_proxy_controller.rb
@@ -5,6 +5,8 @@ class ApiProxyController < ApplicationController
     url = "https://torre.bio/api/bios/#{params[:username]}"
     uri = URI(url)
     response = HTTParty.get(uri)
+    response.headers['X-Requested-With'] = 'XMLHttpRequest'
+    response.headers['Access-Control-Allow-Origin'] = 'https://torre.bio/api/bios/'
     puts response
     render json: response
   end
